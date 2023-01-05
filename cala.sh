8246a4e21485416d444aa2c71e210c4cfd202aa7
Jlll1 <arghantentua@tutanota.com>
2023-01-05 17:48:42 +0100

    Implement adding a process

--- cala.sh
+++ cala.sh
@@ -1,9 +1,95 @@
 #!/bin/sh
 
 CALAPATH=~/.cala
-if [ ! -d $CALAPATH ]; then
-  mkdir -p $CALAPATH
-fi
 
+usage() {
+  echo "cala"
+}
 
+ensurePath() {
+  if [ ! -d $CALAPATH ]; then
+    mkdir -p $CALAPATH
+  fi
+}
+
+getCalaId() {
+  declare -n _out=$1
+
+  local idPath=${CALAPATH}/.id
+  if [ ! -e $idPath ]; then
+    touch $idPath
+  fi
+
+  local id=$(wc -l $idPath | awk '{print $1}')
+  echo "$id" >> $idPath
+
+  _out=$id
+}
+
+newCalaProcess() {
+  declare -n _out=$1
+
+  local calaId=$2
+  local processPath="${CALAPATH}/${calaId}"
+  if [ -e $processPath ]; then
+    echo "process with calaId $calaId already exists"
+    exit 1
+  fi
+
+cat << EOT > $processPath
+status: New
+PID: ?
+EOT
+
+  _out=$processPath
+}
+
+setProcessStatus() {
+  local calaId=$1
+  local newStatus=$2
+
+  local process="${CALAPATH}/${calaId}"
+  if [ ! -e $process ]; then
+    echo "process with calaId $calaId does not exist"
+    exit 1
+  fi
+
+  sed -i "1c\status: $newStatus" $process
+}
+
+setProcessPid() {
+  local calaId=$1
+  local newPid=$2
+
+  local process="${CALAPATH}/${calaId}"
+  if [ ! -e $process ]; then
+    echo "process with calaId $calaId does not exist"
+    exit 1
+  fi
+
+  sed -i "2c\PID: $newPid" $process
+}
+
+add() {
+  local calaId
+  getCalaId calaId
+
+  local process
+  newCalaProcess process $calaId
+
+  setProcessStatus $calaId "Active"
+  bash -c $1 >> "${process}.output" && setProcessStatus $calaId "Completed" &
+  pid=$!
+  setProcessPid $calaId $pid
+}
+
+case $1 in
+add)
+add $2
+;;
+
+*)
+usage
+
+esac
 
